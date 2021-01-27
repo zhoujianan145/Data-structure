76c1ce22741a469fd9006a79614ef10aef95f9d8
zhoujianan145 <972935804@qq.com>
2021-01-27 23:14:47 +0800

    队列的实现

---
+++ 2020_1_27_Implementation of queue/Queue.h
@@ -0,0 +1,34 @@
+#pragma once
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+
+typedef int QDataType;
+typedef struct QueueNode
+{
+	struct QueueNode* _next;
+	QDataType _data;
+}QueueNode;
+//尾上插入，把尾指针直接保存起来
+typedef struct Queue
+{
+	 QueueNode* _head;
+	 QueueNode* _tail;
+}Queue;
+
+//初始化
+void QueueInit(Queue *pq);
+//摧毁
+void QueueDestory(Queue *pq);
+//入队列
+void QueuePush(Queue *pq,QDataType x);
+//出队列
+void QueuePop(Queue *pq);
+
+QDataType QueueFront(Queue *pq);
+
+QDataType QueueBack(Queue *pq);
+//是否为空
+int QueueEmpty(Queue *pq);
+//队列大小
+int QueueSize(Queue *pq);
